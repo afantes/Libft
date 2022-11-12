6a0a3c8a7483cf08f6ce17c763e0491523eefa5a
Anas FANTES <anasfantes.dev@gmail.com>
2022-11-12 23:33:52 +0100

    My LibFt
    42cursus - PROJECT 1

---
+++ ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afantes <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/24 22:24:50 by afantes           #+#    #+#             */
+/*   Updated: 2022/10/24 22:30:35 by afantes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	if (s == 0 || f == 0)
+		return (0);
+	str = malloc(ft_strlen(s) * sizeof(char) + 1);
+	if (!str)
+		return (NULL);
+	while (i < ft_strlen(s))
+	{
+		str[i] = (*f)(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
